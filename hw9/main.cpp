199816f3f1439a3abdd4a22679c9d5c14d2149fe
LTurret <a0919329@gmail.com>
2023-11-22 03:32:01 +0800

    new archive

---
+++ hw9/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class Chain;
+class Node {
+    friend Chain;
+
+   public:
+    int value;
+    int &link;
+};
+
+class Chain {
+   public:
+    void insert(int);
+};
+
+int main() {
+    int a = 3;
+    cout << &a;
+}
