aade5dab0c69972407402b98b06e37504fbff2a8
LTurret <a0919329@gmail.com>
2023-11-21 16:46:14 +0800

    rewrite, overload >>

--- hw6/hw6.cpp
+++ hw6/hw6.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <istream>
 #include <ostream>
 
 using namespace std;
@@ -9,6 +10,7 @@ class Polynomial;
 class Term {
     friend Polynomial;
     friend ostream &operator<<(ostream &os, const Polynomial &polynomial);
+    friend istream &operator>>(istream &in, Polynomial &polynomial);
 
   private:
     float coef;
@@ -17,13 +19,13 @@ class Term {
 
 class Polynomial {
     friend ostream &operator<<(ostream &os, const Polynomial &polynomial);
+    friend istream &operator>>(istream &in, Polynomial &polynomial);
 
   public:
     Polynomial();
     Polynomial Add(Polynomial polynomial);
     Polynomial Mult(Polynomial polynomial);
     float Eval(const float parameter);
-    void new_term(const float coef, const int exp);
 
   private:
     Term *term_array;
@@ -33,25 +35,81 @@ class Polynomial {
 
 ostream &operator<<(ostream &os, const Polynomial &polynomial) {
     for (int i = 0; i < polynomial.terms; i++) {
-        os << "(" << polynomial.term_array[i].coef << ", " << polynomial.term_array[i].exp << ")";
+        if (polynomial.term_array[i].coef != 0) {
+            os << "(" << polynomial.term_array[i].coef << ", " << polynomial.term_array[i].exp << ")";
+        }
     }
     return os;
 }
 
-Polynomial::Polynomial() : capacity(1), terms(0) { term_array = new Term[capacity]; }
+istream &operator>>(istream &in, Polynomial &polynomial) {
+    float coef;
+    int exp;
+
+    in >> coef >> exp;
+
+    if (polynomial.terms == polynomial.capacity) {
+        int new_size = polynomial.capacity * 2;
+        Term *temp = new Term[new_size];
+        copy(polynomial.term_array, polynomial.term_array + polynomial.terms, temp);
+        polynomial.capacity = new_size;
+        delete[] polynomial.term_array;
+        polynomial.term_array = temp;
+    }
+
+    polynomial.term_array[polynomial.terms].coef = coef;
+    polynomial.term_array[polynomial.terms++].exp = exp;
+
+    return in;
+}
+
+Polynomial::Polynomial() : capacity(4), terms(0) { term_array = new Term[capacity]; }
 
 Polynomial Polynomial::Add(Polynomial polynomial) {
     Polynomial result;
     int p1 = 0, p2 = 0;
     while ((p1 <= this->terms) && (p2 <= polynomial.terms)) {
         if ((this->term_array[p1].exp) == (polynomial.term_array[p2].exp)) {
-            result.new_term((this->term_array[p1].coef + polynomial.term_array[p2].coef), this->term_array[p1].exp);
+            float coef = this->term_array[p1].coef + polynomial.term_array[p2].coef;
+            int exp = this->term_array[p1].exp;
+            if (result.terms == result.capacity) {
+                int new_size = result.capacity * 2;
+                Term *temp_array = new Term[new_size];
+                copy(result.term_array, result.term_array + result.terms, temp_array);
+                result.capacity = new_size;
+                delete[] result.term_array;
+                result.term_array = temp_array;
+            }
+            result.term_array[result.terms].coef = coef;
+            result.term_array[result.terms++].exp = exp;
             p1++, p2++;
         } else if ((this->term_array[p1].exp) > (polynomial.term_array[p2].exp)) {
-            result.new_term((this->term_array[p1].exp), (this->term_array[p1].exp));
+            float coef = this->term_array[p1].coef;
+            int exp = this->term_array[p1].exp;
+            if (result.terms == result.capacity) {
+                int new_size = result.capacity * 2;
+                Term *temp_array = new Term[new_size];
+                copy(result.term_array, result.term_array + result.terms, temp_array);
+                result.capacity = new_size;
+                delete[] result.term_array;
+                result.term_array = temp_array;
+            }
+            result.term_array[result.terms].coef = coef;
+            result.term_array[result.terms++].exp = exp;
             p1++;
         } else {
-            result.new_term((polynomial.term_array[p2].coef), (polynomial.term_array[p2].exp));
+            float coef = polynomial.term_array[p2].coef;
+            int exp = polynomial.term_array[p2].exp;
+            if (result.terms == result.capacity) {
+                int new_size = result.capacity * 2;
+                Term *temp_array = new Term[new_size];
+                copy(result.term_array, result.term_array + result.terms, temp_array);
+                result.capacity = new_size;
+                delete[] result.term_array;
+                result.term_array = temp_array;
+            }
+            result.term_array[result.terms].coef = coef;
+            result.term_array[result.terms++].exp = exp;
             p2++;
         }
     }
@@ -62,7 +120,16 @@ Polynomial Polynomial::Mult(Polynomial polynomial) {
     Polynomial result;
     int maximun = polynomial.term_array[0].exp + this->term_array[0].exp;
     for (int i = maximun; i >= 0; i--) {
-        result.new_term(0, i);
+        if (result.terms == result.capacity) {
+            int new_size = result.capacity * 2;
+            Term *temp_array = new Term[new_size];
+            copy(result.term_array, result.term_array + result.terms, temp_array);
+            result.capacity = new_size;
+            delete[] result.term_array;
+            result.term_array = temp_array;
+        }
+        result.term_array[result.terms].coef = 0;
+        result.term_array[result.terms++].exp = i;
     }
     for (int i = 0; i < this->terms; i++) {
         for (int j = 0; j < polynomial.terms; j++) {
@@ -74,7 +141,6 @@ Polynomial Polynomial::Mult(Polynomial polynomial) {
 }
 
 float Polynomial::Eval(const float parameter) {
-    // remember do not just leave a blank variable, it will cause chaos.
     float result = 0.0;
     for (int i = 0; i < terms; i++) {
         result += term_array[i].coef * pow(parameter, term_array[i].exp);
@@ -82,33 +148,37 @@ float Polynomial::Eval(const float parameter) {
     return result;
 }
 
-void Polynomial::new_term(const float coef, const int exp) {
-    if (terms == capacity) {
-        int new_size = capacity * 2;
-        Term *temp_array = new Term[new_size];
-        copy(term_array, term_array + terms, temp_array);
-        capacity = new_size;
-        delete[] term_array;
-        term_array = temp_array;
-    }
-
-    term_array[terms].coef = coef;
-    term_array[terms++].exp = exp;
-}
-
 int main() {
     Polynomial p1, p2;
-    p1.new_term(2, 2);
-    p1.new_term(3, 1);
-    p1.new_term(-2, 0);
+    int c1, c2;
+    cout << "determin c1: ";
+    cin >> c1;
+    cout << "p1: ";
+    for (int i = 0; i < c1; i++) {
+        cin >> p1;
+    }
+    cout << p1 << endl;
 
-    p2.new_term(4, 3);
-    p2.new_term(12, 2);
-    p2.new_term(3, 1);
-    cout << "p1: " << p1 << ", p2: " << p2 << endl;
-    cout << p1.Eval(3) << endl;
+    cout << "determin c2: ";
+    cin >> c2;
+    cout << "p2: ";
+    for (int i = 0; i < c2; i++) {
+        cin >> p2;
+    }
 
-    cout << p1.Add(p2) << endl;
+    cout << p2 << endl;
 
-    cout << p1.Mult(p2) << endl;
+    // p1.new_term(2, 2);
+    // p1.new_term(3, 1);
+    // p1.new_term(-2, 0);
+
+    // p2.new_term(4, 3);
+    // p2.new_term(12, 2);
+    // p2.new_term(3, 1);
+
+    cout << endl << "p1: " << p1 << ", p2: " << p2 << endl << endl;
+
+    cout << "Add: " << p1.Add(p2) << endl;
+    cout << "Mult: " << p1.Mult(p2) << endl;
+    cout << "Eval: " << p1.Eval(3) << endl;
 }
