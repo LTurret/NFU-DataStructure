6d44d00f4ae03e052d25a6b3a25838ea36df14e1
LTurret <a0919329@gmail.com>
2023-11-28 00:50:05 +0800

    rename & ChainNode added

---
+++ hw9/hw9_chainnode.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+
+  public:
+    int data;
+    Node *next;
+};
+
+class ChainNode {
+
+  public:
+    ChainNode(int data, ChainNode *next);
+    void Create2();
+    int data;
+    Node *next;
+};
+
+ChainNode::ChainNode(int data = 0, ChainNode *next = 0) {
+    data = data;
+    next = next;
+}
+
+void ChainNode::Create2() {
+    data = data;
+    ChainNode *second = new ChainNode(10, 0);
+    ChainNode first(20, second);
+}
+
+int main() {
+    ChainNode first;
+    first.Create2();
+}
