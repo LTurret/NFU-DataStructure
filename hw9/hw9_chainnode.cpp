9aa42a53cce427753bfcf72b82b5b03e639b8871
LTurret <a0919329@gmail.com>
2023-11-28 10:54:17 +0800

    code improvements

--- hw9/hw9_chainnode.cpp
+++ hw9/hw9_chainnode.cpp
@@ -2,29 +2,21 @@
 
 using namespace std;
 
+class ChainNode;
+class Chain {
+   public:
+    Chain() { first = 0; };
+    ChainNode *first;
+};
+
 class ChainNode {
    public:
-    ChainNode(int data = 0, ChainNode *next = 0);
-    void Create2();
+    ChainNode(int data = 0) : next(0) { this->data = data; };
     int data;
     ChainNode *next;
 };
 
-ChainNode::ChainNode(int data, ChainNode *next) {
-    this->data = data;
-    this->next = next;
-}
-
-void ChainNode::Create2() {
-    ChainNode *second = new ChainNode(10, 0);
-    next = second;
-}
-
 int main() {
-    ChainNode head;
-    cout << head.data << endl;
-
-    head.Create2();
-    head = *head.next;
-    cout << head.data << endl;
+    ChainNode n1(21);
+    cout << n1.data;
 }
